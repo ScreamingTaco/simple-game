b10b37fb27bead9b94a9094bca0860e4581bc998
ScreamingTaco <fwcarlitos@icloud.com>
2016-07-30 19:13:05 +0000

    Update and rename game.h to src/game.h

---
+++ src/game.h
@@ -0,0 +1,36 @@
+/*  Adventures of Linus- text based adventure game
+    Copyright (C) 2016  Carlos Vazquez
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact me at fwcarlitos@icloud.com*/
+
+
+#ifndef game_h
+#define game_h
+
+#include "fighters.h"
+
+class game {
+	public:
+		void play(); //this should be the only public function
+	private: // everything not in fighters.h goes here
+        void show_story() const;
+        FIGHTER_TYPE calc_Type(char input);
+		void battle(player &battle_player, enemy &battle_enemy);
+        void show_help() const;
+        void show_credits() const;
+};
+
+#endif
